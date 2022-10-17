3514abbe39e67d4e81997e185444593f5fac662a
ariantershnjaku <114836852+ariantershnjaku@users.noreply.github.com>
2022-10-17 23:23:17 +0200

    Removed foreign key user

--- Notetaking/Notetaking/Models/DomainModels/Note.cs
+++ Notetaking/Notetaking/Models/DomainModels/Note.cs
@@ -1,17 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-
 namespace Notetaking.Models.DomainModels
 {
-    public class Note
-    { 
+    public class Note{ 
         public Guid Id { get; set; }
         public string? Heading { get; set; }
         public string? Text { get; set; }
         public DateTime DateCreated { get; set; }
         
-        [ForeignKey("UserId")]
-        public User? User { get; set; } 
     }
 }
